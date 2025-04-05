15d29a71f41bc46e8421ee9e13882b3b1f786b74
Gambhir ⚡ <gambhir786191ss@gmail.com>
2025-04-05 02:25:03 +0200

    feat: google analytics

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import UnoCSS from 'unocss/astro'
 import vue from '@astrojs/vue'
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://gambhir.xyz',
@@ -16,6 +17,11 @@ export default defineConfig({
       injectReset: true,
     }),
     vue(),
+    partytown({ 
+      config: { 
+        forward: ['dataLayer.push'] 
+      } 
+    }),
   ],
   markdown: {
     shikiConfig: {
